89893ca1387ab5ddd308d6434564fb2114809fb5
망꼬 <130226437+runmanggo@users.noreply.github.com>
2023-11-07 00:19:09 +0900

    파이어베이스 오류 해결
    - 파이어베이스 재설정
    - 데이터 연결 완료

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -3,119 +3,59 @@ import Slider from "../components/Slider/Slider";
 import Filter from "../components/Filter/Filter";
 import Banner from "../components/Banner/Banner";
 
+import classes from "../style/category.module.css";
+import Imggg from "../assets/image/PLP_perfume_chamo_50ml.jpg";
+
 const Category = () => {
   return (
     <div>
       <Slider />
       <Banner />
       <Filter />
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui aliquid,
-        minima facilis laborum facere, impedit ipsa nobis cumque magnam tempora
-        officiis quidem distinctio accusamus odit magni. Harum laudantium
-        aspernatur incidunt. Labore eius earum omnis aliquid sit odit quisquam
-        quas debitis, voluptate repudiandae veniam atque molestiae rerum nostrum
-        repellat fugit nobis ullam, ipsum, laborum adipisci libero cumque odio.
-        Obcaecati, debitis architecto. Ad numquam magni, quis neque rem, odio
-        impedit perspiciatis nihil soluta dignissimos enim. Saepe nobis,
-        officiis fugiat sunt doloremque voluptates odio cupiditate officia
-        natus, nihil exercitationem provident laboriosam quidem perferendis.
-        Quia saepe, tenetur expedita repellendus inventore non aliquam adipisci
-        nobis repellat placeat quae, repudiandae hic alias corrupti assumenda
-        eos vel blanditiis dolores esse ipsa natus iusto officia. Repellendus, a
-        necessitatibus? Sit praesentium corrupti quia totam eius dolores,
-        commodi et deleniti illo deserunt, saepe, repellat sapiente! Laudantium
-        praesentium optio temporibus earum repudiandae fugit, porro amet iure
-        minima cupiditate sequi dicta asperiores? Inventore doloremque soluta
-        placeat impedit perferendis ducimus minus illum maxime fugiat
-        reprehenderit numquam nulla veritatis, omnis minima, quisquam assumenda,
-        earum eius? Praesentium ipsam harum ea aspernatur recusandae id esse
-        aliquid. Impedit consequatur assumenda nihil iste natus a modi
-        similique, repellendus eaque ex! Impedit, esse dolorem necessitatibus
-        nesciunt enim soluta est magni error corporis saepe distinctio qui illum
-        minus, quo adipisci. Sunt iste nobis placeat necessitatibus a expedita
-        commodi fugiat, alias ipsam at, iusto quo, ipsa laborum? Dignissimos
-        ipsa corporis perspiciatis architecto magnam eaque quod, libero sit
-        saepe consequatur adipisci quam? Quisquam adipisci neque repellendus.
-        Quaerat quidem debitis sint dolorum aliquam distinctio iste quia libero
-        vero. Minima corporis veniam voluptates voluptate, beatae eos facere
-        dolor tempore, qui libero at eaque numquam. Praesentium temporibus
-        assumenda accusamus expedita inventore consectetur natus autem nulla
-        necessitatibus ratione officiis sequi placeat facere architecto,
-        molestiae asperiores nostrum laboriosam sit accusantium id deleniti
-        dolor? Quos consequuntur sit neque?
+      <div className={classes.ctgList__container}>
+        <div className={classes.ctgList__inner}>
+          <div className={classes.ctgLis__productBox}>
+            <div className={classes.ctgLis__img}>
+              <img src={Imggg} alt="" />
+            </div>
+            <div className={classes.ctgLis__info}>
+              <div className={classes.ctgLis__info__itemDesc}>
+                진득한 카모마일 | 부드러운 나무결 | 머스크
+              </div>
+              <div className={classes.ctgLis__info__title}>퍼퓸 카모 </div>
+              <div className={classes.ctgLis__info__price}>
+                <span className={classes.ctgLis__price}>139,000 원 </span>
+                <span className={classes.ctgLis__capacity}>50mL</span>
+              </div>
+
+              <span className={classes.ctgList__size}>
+                +<strong>3</strong> Sizes
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui aliquid,
-        minima facilis laborum facere, impedit ipsa nobis cumque magnam tempora
-        officiis quidem distinctio accusamus odit magni. Harum laudantium
-        aspernatur incidunt. Labore eius earum omnis aliquid sit odit quisquam
-        quas debitis, voluptate repudiandae veniam atque molestiae rerum nostrum
-        repellat fugit nobis ullam, ipsum, laborum adipisci libero cumque odio.
-        Obcaecati, debitis architecto. Ad numquam magni, quis neque rem, odio
-        impedit perspiciatis nihil soluta dignissimos enim. Saepe nobis,
-        officiis fugiat sunt doloremque voluptates odio cupiditate officia
-        natus, nihil exercitationem provident laboriosam quidem perferendis.
-        Quia saepe, tenetur expedita repellendus inventore non aliquam adipisci
-        nobis repellat placeat quae, repudiandae hic alias corrupti assumenda
-        eos vel blanditiis dolores esse ipsa natus iusto officia. Repellendus, a
-        necessitatibus? Sit praesentium corrupti quia totam eius dolores,
-        commodi et deleniti illo deserunt, saepe, repellat sapiente! Laudantium
-        praesentium optio temporibus earum repudiandae fugit, porro amet iure
-        minima cupiditate sequi dicta asperiores? Inventore doloremque soluta
-        placeat impedit perferendis ducimus minus illum maxime fugiat
-        reprehenderit numquam nulla veritatis, omnis minima, quisquam assumenda,
-        earum eius? Praesentium ipsam harum ea aspernatur recusandae id esse
-        aliquid. Impedit consequatur assumenda nihil iste natus a modi
-        similique, repellendus eaque ex! Impedit, esse dolorem necessitatibus
-        nesciunt enim soluta est magni error corporis saepe distinctio qui illum
-        minus, quo adipisci. Sunt iste nobis placeat necessitatibus a expedita
-        commodi fugiat, alias ipsam at, iusto quo, ipsa laborum? Dignissimos
-        ipsa corporis perspiciatis architecto magnam eaque quod, libero sit
-        saepe consequatur adipisci quam? Quisquam adipisci neque repellendus.
-        Quaerat quidem debitis sint dolorum aliquam distinctio iste quia libero
-        vero. Minima corporis veniam voluptates voluptate, beatae eos facere
-        dolor tempore, qui libero at eaque numquam. Praesentium temporibus
-        assumenda accusamus expedita inventore consectetur natus autem nulla
-        necessitatibus ratione officiis sequi placeat facere architecto,
-        molestiae asperiores nostrum laboriosam sit accusantium id deleniti
-        dolor? Quos consequuntur sit neque?
-      </div>
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui aliquid,
-        minima facilis laborum facere, impedit ipsa nobis cumque magnam tempora
-        officiis quidem distinctio accusamus odit magni. Harum laudantium
-        aspernatur incidunt. Labore eius earum omnis aliquid sit odit quisquam
-        quas debitis, voluptate repudiandae veniam atque molestiae rerum nostrum
-        repellat fugit nobis ullam, ipsum, laborum adipisci libero cumque odio.
-        Obcaecati, debitis architecto. Ad numquam magni, quis neque rem, odio
-        impedit perspiciatis nihil soluta dignissimos enim. Saepe nobis,
-        officiis fugiat sunt doloremque voluptates odio cupiditate officia
-        natus, nihil exercitationem provident laboriosam quidem perferendis.
-        Quia saepe, tenetur expedita repellendus inventore non aliquam adipisci
-        nobis repellat placeat quae, repudiandae hic alias corrupti assumenda
-        eos vel blanditiis dolores esse ipsa natus iusto officia. Repellendus, a
-        necessitatibus? Sit praesentium corrupti quia totam eius dolores,
-        commodi et deleniti illo deserunt, saepe, repellat sapiente! Laudantium
-        praesentium optio temporibus earum repudiandae fugit, porro amet iure
-        minima cupiditate sequi dicta asperiores? Inventore doloremque soluta
-        placeat impedit perferendis ducimus minus illum maxime fugiat
-        reprehenderit numquam nulla veritatis, omnis minima, quisquam assumenda,
-        earum eius? Praesentium ipsam harum ea aspernatur recusandae id esse
-        aliquid. Impedit consequatur assumenda nihil iste natus a modi
-        similique, repellendus eaque ex! Impedit, esse dolorem necessitatibus
-        nesciunt enim soluta est magni error corporis saepe distinctio qui illum
-        minus, quo adipisci. Sunt iste nobis placeat necessitatibus a expedita
-        commodi fugiat, alias ipsam at, iusto quo, ipsa laborum? Dignissimos
-        ipsa corporis perspiciatis architecto magnam eaque quod, libero sit
-        saepe consequatur adipisci quam? Quisquam adipisci neque repellendus.
-        Quaerat quidem debitis sint dolorum aliquam distinctio iste quia libero
-        vero. Minima corporis veniam voluptates voluptate, beatae eos facere
-        dolor tempore, qui libero at eaque numquam. Praesentium temporibus
-        assumenda accusamus expedita inventore consectetur natus autem nulla
-        necessitatibus ratione officiis sequi placeat facere architecto,
-        molestiae asperiores nostrum laboriosam sit accusantium id deleniti
-        dolor? Quos consequuntur sit neque?
+      {/* 확인용 */}
+      <div className={classes.ctgList__inner}>
+        <div className={classes.ctgLis__productBox}>
+          <div className={classes.ctgLis__img}>
+            <img src={Imggg} alt="" />
+          </div>
+          <div className={classes.ctgLis__info}>
+            <div className={classes.ctgLis__info__itemDesc}>
+              진득한 카모마일 | 부드러운 나무결 | 머스크
+            </div>
+            <div className={classes.ctgLis__info__title}>퍼퓸 카모 </div>
+            <div className={classes.ctgLis__info__price}>
+              <span className={classes.ctgLis__price}>139,000 원 </span>
+              <span className={classes.ctgLis__capacity}>50mL</span>
+            </div>
+
+            <span className={classes.ctgList__size}>
+              +<strong>3</strong> Sizes
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );
