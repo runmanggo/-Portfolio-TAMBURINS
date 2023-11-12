beb1f0043e42ba884f5fe2e41a68ff2da8389dfd
runmanggo <runmanggo@gmail.com>
2023-11-12 19:08:30 +0900

    Design : 수정 css, 미디어쿼리 적용

--- client/src/style/StyledComponents.js
+++ client/src/style/StyledComponents.js
@@ -8,10 +8,20 @@ export const CtgLsitContainer = styled.div`
   color: var(--black);
   margin: 0 auto;
   padding: 0 2.75rem;
+
+  @media (max-width: 1024px) {
+    padding: 0;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 3px;
+  }
+`;
+
+export const BtnBox = styled.div`
+  padding: 0 20px;
 `;
 
 export const AllBtn = styled.button`
-  margin: 20px auto 200px;
+  margin: 20px auto 150px;
   display: block;
   width: 335px;
   height: 45px;
@@ -19,5 +29,9 @@ export const AllBtn = styled.button`
   text-align: center;
   border: 1px solid #a8a8a8;
   font-size: 12px;
-  font-weight: 500;
+  font-weight: 400;
+  @media (max-width: 1024px) {
+    width: 100%;
+    margin: 20px auto 80px;
+  }
 `;
