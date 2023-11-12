bc21d9707c85442827641b187d55062a5d528d6e
runmanggo <runmanggo@gmail.com>
2023-11-12 16:34:26 +0900

    Add : 업데이트  ItemCard에 해당 되는 카테고리를 AllBtn 컴포넌트 표시
    categoryItems의 ctgId 와 allItems를 4개씩 묶은 itemGroup의 ctgId를 비교하여 일치할때 해당 데이터가 표시가 되게 구성

--- client/src/pages/Shop.jsx
+++ client/src/pages/Shop.jsx
@@ -22,11 +22,31 @@ const fetchAll = async () => {
   }
 };
 
-const Shop = () => {
-  const { data: allItems, isLoading, error } = useQuery("allItems", fetchAll);
+const fetchTitle = async () => {
+  try {
+    const response = await axios.get("http://localhost:8000/categories");
+    const data = response.data;
+    return data;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
 
-  if (isLoading) return console.log("로딩중");
-  if (error) return console.log(error.message);
+const Shop = () => {
+  const {
+    data: allItems,
+    isLoading: isLoadingAllItems,
+    error: allItemsError,
+  } = useQuery("allItems", fetchAll);
+  const {
+    data: categoryItems,
+    isLoading: isLoadingCategoryItems,
+    error: categoryItemsError,
+  } = useQuery("categoryItems", fetchTitle);
+
+  if (isLoadingAllItems || isLoadingCategoryItems) return console.log("로딩중");
+  if (allItemsError || categoryItemsError)
+    return console.log(allItemsError?.message || categoryItemsError?.message);
 
   return (
     <div>
@@ -39,7 +59,18 @@ const Shop = () => {
               <ItemCard key={item._id} item={item} />
             ))}
           </CtgLsitContainer>
-          <AllBtn>{allItems[index].category} 모두보기</AllBtn>
+          {categoryItems.some(
+            (category) => category.ctgId === itemGroup[0].ctgId
+          ) && (
+            <AllBtn>
+              {
+                categoryItems.find(
+                  (category) => category.ctgId === itemGroup[0].ctgId
+                ).sliderTitle
+              }
+              모두보기
+            </AllBtn>
+          )}
         </React.Fragment>
       ))}
     </div>
