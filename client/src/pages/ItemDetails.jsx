c3aa5fee23aa97a5cc29b6e8a12e95289fdee65e
runmanggo <runmanggo@gmail.com>
2023-11-13 20:10:35 +0900

    Feat : 추가 상품정보 모달창

--- client/src/pages/ItemDetails.jsx
+++ client/src/pages/ItemDetails.jsx
@@ -45,10 +45,15 @@ const products = [
 const ItemDetails = () => {
   const [isShown, setIsShown] = useState(true);
 
+  const [isActive, setIsActive] = useState(false);
+
   const handleBtnClick = () => {
     setIsShown(!isShown);
   };
 
+  const handleClick = () => {
+    setIsActive(!isActive);
+  };
   return (
     <>
       <div className={classes.detail__content}>
@@ -74,11 +79,15 @@ const ItemDetails = () => {
             <div className={classes.product__sizeBox}>
               <ul className={classes.product__size}>
                 <li>
-                  <img src={Img1} alt="" />
+                  <div className={classes.product__size__border}>
+                    <img src={Img1} alt="" />
+                  </div>
                   <div>11</div>
                 </li>
                 <li>
-                  <img src={Img1} alt="" />
+                  <div className={classes.product__size__border}>
+                    <img src={Img1} alt="" />
+                  </div>
                   <div>11</div>
                 </li>
               </ul>
@@ -95,26 +104,34 @@ const ItemDetails = () => {
                 나무결의 블론드 우드와 따뜻한 머스크로 감싸주어 당신의 지친
                 마음에 특별하고 작은 위안을 선물합니다.
               </div>
-              <button className={classes.detail__btn} onClick={handleBtnClick}>
+              <button
+                style={{ display: isShown ? "block" : "none" }}
+                className={classes.detail__btn}
+                onClick={handleBtnClick}
+              >
                 퍼퓸 더보기
               </button>
               <div
                 style={{ display: isShown ? "none" : "block" }}
                 className={classes.detail__info__ctg__summary}
               >
-                <strong>탬버린즈 퍼퓸</strong>
-                <br />
-                탬버린즈가 찾아낸 규정되지 않은 아름다움. 시간이 흐르면서
-                일어나는 다채로운 향의 변화를 느껴보세요. 세상에 흩어진 모든
-                이야기에서 영감을 받은 감각적인 향이 단조로운 일상에 자유롭고
-                새로운 리듬을 부여합니다.
-                <br />
-                <br />
-                <strong>탬버린즈의 미학</strong>
-                <br />
-                직선적으로 느껴지는 날카로운 첫인상과 대비되는 부드러운 곡선의
-                촉각적인 경험은 손끝으로 사물을 읽는 듯한 공감각적인 흥미로움을
-                선사합니다.
+                <div>
+                  <span>탬버린즈 퍼퓸</span>
+                  <p className={classes.detail__info__ctg__summary__p}>
+                    탬버린즈가 찾아낸 규정되지 않은 아름다움. 시간이 흐르면서
+                    일어나는 다채로운 향의 변화를 느껴보세요. 세상에 흩어진 모든
+                    이야기에서 영감을 받은 감각적인 향이 단조로운 일상에
+                    자유롭고 새로운 리듬을 부여합니다.
+                  </p>
+                </div>
+                <div>
+                  <span>탬버린즈의 미학</span>
+                  <p>
+                    직선적으로 느껴지는 날카로운 첫인상과 대비되는 부드러운
+                    곡선의 촉각적인 경험은 손끝으로 사물을 읽는 듯한 공감각적인
+                    흥미로움을 선사합니다.
+                  </p>
+                </div>
               </div>
             </div>
 
@@ -122,16 +139,46 @@ const ItemDetails = () => {
 
             <div className={classes.accordian__container}>
               <Accordion title="Section 1">
-                <p>This is the content for section 1.</p>
+                This is the content for section 1.
               </Accordion>
-              <Accordion title="Section 2">
-                <p>This is the content for section 2.</p>
+              <Accordion title="온라인 익스클루시브 혜택">
+                <div>
+                  온라인 익스클루시브 혜택 탬버린즈는 고객님들께 빠른 배송 및
+                  반품과 최고의 경험을 제공하기 위해 언제나 세심한 주의를
+                  기울입니다. 고객님을 위한 익스클루시브 서비스를 경험해보세요.
+                </div>
+                <br />
+                <div>
+                  · 회원가입 및 카카오톡 플러스 친구 추가 시 바로 사용 가능한
+                  3,000원 혜택
+                </div>
+                <div>· 구매 금액의 2% 적립 </div>
+                <div>
+                  · 생일 축하 5,000 포인트 혜택 (1년 이내 구매 이력 있을 시)
+                </div>
               </Accordion>
               <Accordion title="Section 3">
-                <p>This is the content for section 2.</p>
+                This is the content for section 2.
               </Accordion>
-              <Accordion title="Section 3">
-                <p>This is the content for section 4.</p>
+              <Accordion title="배송 & 반품">
+                <div>3만원 이상 구매하실 경우 배송 비용은 무료입니다.</div>
+                <br />
+                <div style={{ color: "red" }}>
+                  <strong>주문일로부터 1-2 영업일 이내 출고됩니다.</strong>
+                </div>
+                <br />
+                <div>
+                  배송은 지역 택배사 사정에 따라 약간의 지연이 생길 수 있습니다.
+                  배송이 시작되면 구매자에게는 이메일, 수령인에게는 카카오
+                  알림톡으로 배송 정보를 전송해 드립니다.
+                  <br />
+                  CJ대한통운(https://www.cjlogistics.com) <br />
+                </div>
+                <br />
+                *상품 혹은 증정품의 포장(랩핑)을 개봉 및 훼손한 경우 반품이
+                불가합니다.
+                <br /> *단순 변심 또는 주문 실수로 인한 교환이 불가합니다.
+                신중한 구매 부탁드립니다.
               </Accordion>
             </div>
           </div>
