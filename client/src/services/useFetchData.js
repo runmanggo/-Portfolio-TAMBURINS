1e14ce5835f5ca44d6e3b4a939f6b3e9c800332c
runmanggo <runmanggo@gmail.com>
2023-11-26 01:52:22 +0900

    Feat : 추가 데이터 api 커스텀 훅 변형
    - 단순 데이터 받아오는 api는 커스텀 훅 변형하여 해당 url 만 파라미터로 받아서 재사용성을 올림

---
+++ client/src/services/useFetchData.js
@@ -0,0 +1,22 @@
+import { useState, useCallback } from "react";
+import axios from "axios";
+
+export const useFetchData = (url) => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = useCallback(async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get(url);
+      setData(response.data);
+      setLoading(false);
+    } catch (error) {
+      setError(error);
+      setLoading(false);
+    }
+  }, [url]);
+
+  return { data, error, loading, fetchData };
+};
